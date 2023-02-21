aeda224066f9e8045c297023c00d4923d4a3880a
Mads Emil <mads81i3@stud.kea.dk>
2023-02-21 12:29:36 +0100

    spotlight scrol done

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ inView(".pierre", () => {
   animate(".facts_animation", { y: [+2000, -650] }, { duration: 3.5 });
 });
 
-inView(".spotlight_1", () => {
-  animate(".light", { opacity: 1 }, { duration: 10 });
+// inView(".spotlight_1", () => {
+//   animate(".light", { opacity: 1 }, { duration: 10 });
+// });
+
+document.querySelectorAll("article > div").forEach((light) => {
+  scroll(animate(light, { opacity: [0, 1, 1, 0] }), {
+    target: light,
+    offset: ["start end", "end end", "start start", "end start"],
+  });
 });
