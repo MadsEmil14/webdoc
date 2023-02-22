bd32817bc5f717b5393799dbc328735d850654e9
Mads Emil <mads81i3@stud.kea.dk>
2023-02-22 19:16:43 +0100

    aniamtion på filmrulle 80% + lomo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
+import { info } from "autoprefixer";
 
 inView(".headline_animation", () => {
   animate(".headline_animation", { y: [+2000, 0] }, { duration: 2.5 });
@@ -18,4 +19,20 @@ document.querySelectorAll("article > div").forEach((light) => {
     target: light,
     offset: ["start end", "end end", "start start", "end start"],
   });
+});
+
+// inView(".filmrulle_stock", () => {
+//   animate(".filmrulle_tekst_mobil", { y: [+2000, 0] }, { duration: 3.5 });
+// });
+
+inView(".filmrulle_tekst_mobil", () => {
+  animate(
+    ".filmrulle_tekst_mobil",
+    { opacity: [0, 0.3, 0.5, 0.7, 1] },
+    { duration: 5 }
+  );
+});
+
+inView(".filmrulle_stock_web", () => {
+  animate(".filmrulle_tekst_web", { x: [-2000, 0] }, { duration: 3.5 });
 });
