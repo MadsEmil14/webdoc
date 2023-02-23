7434edad11e93ebc3c8f6bce5ba2c484679a6bde
Mads Emil <mads81i3@stud.kea.dk>
2023-02-23 16:11:57 +0100

    commit mig i numsen

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 import { info } from "autoprefixer";
 
-// inView(".headline", () => {
-//   animate(".headline", { y: [+2000, 0] }, { duration: 2.5 });
-// });
-
 inView(".headline", () => {
   animate(".headline", { opacity: [0, 1] }, { duration: 5 });
 });
@@ -18,20 +14,12 @@ inView(".pierre", () => {
   animate(".facts_animation", { y: [+2000, -650] }, { duration: 3.5 });
 });
 
-// inView(".spotlight_1", () => {
-//   animate(".light", { opacity: 1 }, { duration: 10 });
-// });
-
 document.querySelectorAll("article > div").forEach((light) => {
   scroll(animate(light, { opacity: [0, 1, 1, 0] }), {
     target: light,
     offset: ["start end", "end end", "start start", "end start"],
   });
 });
-
-// inView(".filmrulle_stock", () => {
-//   animate(".filmrulle_tekst_mobil", { y: [+2000, 0] }, { duration: 3.5 });
-// });
 
 inView(".filmrulle_tekst_mobil", () => {
   animate(
@@ -43,4 +31,8 @@ inView(".filmrulle_tekst_mobil", () => {
 
 inView(".filmrulle_stock_web", () => {
   animate(".filmrulle_tekst_web", { x: [+2000, 0] }, { duration: 3.5 });
+});
+
+inView(".filmrulle_analog", () => {
+  animate(".filmrulle_tekst_analog_web", { x: [-2000, 0] }, { duration: 3.5 });
 });
